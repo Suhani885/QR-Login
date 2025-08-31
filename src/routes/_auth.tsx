66c587766e8121423858b4c614669cccaba64661
prince <prince121kk@gmail.com>
2025-08-31 11:19:19 +0530

    merge origin/main

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -54,20 +54,19 @@ function homeLayout() {
 
   if (loading) {
     return (
-    <div>
-      <CutoutTextLoader
-        height="900px"
-        background="black"
-        imgUrl="https://images.unsplash.com/photo-1756370473190-4c41ddbd5e59?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
-      />
-    </div>
-  );
+      <div>
+        <CutoutTextLoader
+          height="1100px"
+          background="black"
+          imgUrl="https://images.unsplash.com/photo-1756370473190-4c41ddbd5e59?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
+        />
+      </div>
+    );
   }
   if (isError) {
     router.navigate({ to: "/" })
     console.log(data)
   }
-
   return <>
     <Outlet />
   </>
