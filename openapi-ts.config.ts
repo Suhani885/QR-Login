b25610566484705cab46ebb8e44362e9ca0f55ed
prince <prince121kk@gmail.com>
2025-09-01 12:25:45 +0530

    feat: sse

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-    input: "http://10.21.97.249:8001/api/schema",
+    input: "http://10.21.99.223:8001/api/schema/",
     output: 'src/services/api',
     plugins: [
         '@tanstack/react-query',
